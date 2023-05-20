09da67b70e3dd6a856db14d5218ef0ed1c7bd473
selunders <sethlunders@gmail.com>
2023-05-19 18:53:37 -0700

    Initial build of site

---
+++ _site/js/helpers.js
@@ -0,0 +1,61 @@
+const fs = require('fs');
+const Image = require("@11ty/eleventy-img");
+
+function minifyPhotos(){
+    const photosDir = "img/home"
+    let photos = fs.readdirSync(photosDir);
+    for (let i in photos) {
+        (async () => {
+            let photo = `${photosDir}/${photos[i]}`;
+            console.log(`Photo: ${photo}`);
+            const url = photo;
+            const stats = await Image(url, {
+                formats: ["webp"],
+                widths: [600],
+                // dryRun: true,
+                outputDir: `${photosDir}-min/`,
+            });
+            // console.log(stats);
+        })();
+    }
+}
+
+class PhotoData {
+    constructor(){
+        // this.url = url
+        this.title = "";
+        this.description = "";
+    }
+    // function UpdateURL(url)
+    // {
+    //     this.url = url;
+    // }
+}
+
+function createPhotoData(){
+    let photoData
+    try {
+        const photoJSON = fs.readFileSync('./js/imageData.json', {encoding: 'utf-8', flag: 'r'});
+        photoData = JSON.parse(photoJSON)
+    }
+    catch {
+        photoData = new Object();
+    }
+    const photos = fs.readdirSync('./img/home-min');
+    photos.forEach((photo) => {
+        console.log(photoData);
+        if(!photoData[photo])
+        {
+            photoData[photo] = new PhotoData(photo);
+        }
+        else
+        {
+            photoData[photo].up
+        }
+    });
+    console.log(photoData);
+    fs.writeFileSync('./js/imageData.json', JSON.stringify(photoData));
+}
+
+// minifyPhotos();
+createPhotoData();
---
+++ _site/js/home.js
@@ -0,0 +1,70 @@
+let photosData
+
+async function fetchPhotoData() {
+    const response = await fetch('js/imageData.json');
+    const jsonData = await response.json();
+    photosData = jsonData;
+};
+
+// const photos = document.querySelectorAll(".photos img");
+const photoPopup = document.querySelector("section.popup");
+const popupButtons = photoPopup.querySelectorAll("button");
+popupButtons[0].setAttribute("onclick", "previousPhoto()");
+popupButtons[1].setAttribute("onclick", "nextPhoto()");
+popupButtons[2].setAttribute("onclick", "closePopup()");
+photoPopup.addEventListener("click", () => {
+    // alert("Clicked!");
+    // photoPopup.classList.toggle("hide");
+});
+
+fetchPhotoData().then(() => {
+    for(let photo in photosData)
+    {
+        const photoElement = document.querySelector(`[photoname="${photo}"]`)
+        photoElement.photo = photo;
+        photoElement.title = photosData[photo].title;
+        photoElement.description = photosData[photo].description;
+        photoElement.addEventListener("click", () => {
+            selectPhoto(photoElement);
+        })
+    }
+});
+
+function selectPhoto(photoElement) {
+    photoPopup.classList.remove("hide");
+    photoPopup.currentPhoto = photoElement;
+    photoPopup.querySelector("img").src = `img/home-min/${photoElement.photo}`;
+    photoPopup.querySelector("h2").innerText = photoElement.title;
+    photoPopup.querySelector("p").innerText = photoElement.description;
+}
+
+function nextPhoto()
+{
+    if(photoPopup.currentPhoto.nextElementSibling)
+    {
+        selectPhoto(photoPopup.currentPhoto.nextElementSibling);
+    }
+    else
+    {
+        selectPhoto(document.querySelector(".photos img:first-child"));
+    }
+}
+
+function previousPhoto()
+{
+    if(photoPopup.currentPhoto.previousElementSibling)
+    {
+        selectPhoto(photoPopup.currentPhoto.previousElementSibling);
+    }
+    else
+    {
+        selectPhoto(document.querySelector(".photos img:last-child"));
+    }
+}
+
+function closePopup()
+{
+    photoPopup.classList.toggle("hide");
+}
+// const buttons = photoPopup.querySelectorAll("button");
+// // buttons[0].onclick = ""
