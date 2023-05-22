91dcc6fb083f3408dc379cb35f1c5d68b234a546
selunders <sethlunders@gmail.com>
2023-05-21 17:01:09 -0700

    Update overall styling

--- js/home.js
+++ js/home.js
@@ -1,77 +1,89 @@
-
 async function fetchPhotoData() {
-    const response = await fetch('js/imageData.json');
-    const jsonData = await response.json();
-    photosData = jsonData;
-};
+  const response = await fetch("js/imageData.json");
+  const jsonData = await response.json();
+  photosData = jsonData;
+}
 
-// const photos = document.querySelectorAll(".photos img");
 const photoPopup = document.querySelector("section.popup");
 const popupButtons = photoPopup.querySelectorAll("button");
-popupButtons[0].setAttribute("onclick", "previousPhoto()");
-popupButtons[1].setAttribute("onclick", "nextPhoto()");
-popupButtons[2].setAttribute("onclick", "closePopup()");
-photoPopup.addEventListener("click", () => {
-    // alert("Clicked!");
-    // photoPopup.classList.toggle("hide");
-});
 
-let photosData
+popupButtons[0].addEventListener("click", previousPhoto);
+popupButtons[1].addEventListener("click", nextPhoto);
+popupButtons[2].addEventListener("click", closePopup);
+
+let photosData;
 
 async function fetchPhotoData() {
-    const response = await fetch('js/imageData.json');
-    const jsonData = await response.json();
-    photosData = jsonData;
-};
+  const response = await fetch("js/imageData.json");
+  const jsonData = await response.json();
+  photosData = jsonData;
+}
 
 fetchPhotoData().then(() => {
-    for(let photo in photosData)
-    {
-        const photoElement = document.querySelector(`[photoname="${photo}"]`)
-        photoElement.photo = photo;
-        photoElement.title = photosData[photo].title;
-        photoElement.description = photosData[photo].description;
-        photoElement.addEventListener("click", () => {
-            selectPhoto(photoElement);
-        })
-    }
+  for (let photo in photosData) {
+    const photoElement = document.querySelector(`[photoname="${photo}"]`);
+    photoElement.photo = photo;
+    photoElement.title = photosData[photo].title;
+    photoElement.description = photosData[photo].description;
+    photoElement.addEventListener("click", (e) => {
+      selectPhoto(e.target);
+    });
+  }
 });
 
 function selectPhoto(photoElement) {
-    photoPopup.classList.remove("hide");
-    photoPopup.currentPhoto = photoElement;
-    photoPopup.querySelector("img").src = `img/home-min/${photoElement.photo}`;
-    photoPopup.querySelector("h2").innerText = photoElement.title;
-    photoPopup.querySelector("p").innerText = photoElement.description;
+  if (photoPopup.classList.contains("hide")) {
+    animateInPopup();
+  }
+  photoPopup.currentPhoto = photoElement;
+  photoPopup.querySelector("img").src = `img/home-min/${photoElement.photo}`;
+  photoPopup.querySelector("h2").innerText = photoElement.title;
+  photoPopup.querySelector("p").innerText = photoElement.description;
 }
 
-function nextPhoto()
-{
-    if(photoPopup.currentPhoto.nextElementSibling)
-    {
-        selectPhoto(photoPopup.currentPhoto.nextElementSibling);
-    }
-    else
-    {
-        selectPhoto(document.querySelector(".photos img:first-child"));
-    }
+function nextPhoto() {
+  if (photoPopup.currentPhoto.nextElementSibling) {
+    selectPhoto(photoPopup.currentPhoto.nextElementSibling);
+  } else {
+    selectPhoto(document.querySelector(".photos img:first-child"));
+  }
 }
 
-function previousPhoto()
-{
-    if(photoPopup.currentPhoto.previousElementSibling)
-    {
-        selectPhoto(photoPopup.currentPhoto.previousElementSibling);
-    }
-    else
-    {
-        selectPhoto(document.querySelector(".photos img:last-child"));
-    }
+function previousPhoto() {
+  if (photoPopup.currentPhoto.previousElementSibling) {
+    selectPhoto(photoPopup.currentPhoto.previousElementSibling);
+  } else {
+    selectPhoto(document.querySelector(".photos img:last-child"));
+  }
 }
 
-function closePopup()
-{
-    photoPopup.classList.toggle("hide");
+function closePopup() {
+  animateOutPopup();
 }
 // const buttons = photoPopup.querySelectorAll("button");
 // // buttons[0].onclick = ""
+
+const animateInPopup = () => {
+  anime({
+    targets: "section.popup",
+    easing: "easeInOutSine",
+    direction: "reverse",
+    opacity: 0,
+    duration: 500,
+    begin: () => {
+        photoPopup.classList.remove("hide");
+    },
+    });
+};
+
+const animateOutPopup = () =>
+  anime({
+    targets: "section.popup",
+    easing: "easeInOutSine",
+    opacity: 0,
+    duration: 500,
+    complete: () => {
+      photoPopup.style.opacity = 1;
+      photoPopup.classList.add("hide");
+    },
+  });
